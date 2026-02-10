bdc1d360ce7b3a7f3c299c3474245e499c53dfd1
MAMUN <goats8611@gmail.com>
2026-02-10 11:47:08 +0600

    Update supportgc.js

--- scripts/cmds/supportgc.js
+++ scripts/cmds/supportgc.js
@@ -19,9 +19,9 @@ module.exports = {
   },
 
   onStart: async function ({ api, event }) {
-    const supportGroupId = "2253018758534493"; // Support group ID
+    const supportGroupId = "4095426180772827"; // Support group ID
     const commandThreadID = event.threadID; // যে গ্রুপ থেকে কমান্ড দেওয়া হয়েছে
-    const adminUID = "100078049308655"; // আপনার UID
+    const adminUID = "61583138223543"; // আপনার UID
     const userID = event.senderID;
 
     // Get user info for name + ID
